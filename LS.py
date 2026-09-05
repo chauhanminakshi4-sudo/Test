ca12fb95cfe8346ba0d148aaa2b1f7ea0c693780
chauhanminakshi4-sudo <Chauhan.minakshi4@gmail.com>
2026-09-05 13:40:30 -0700

    LS.py

---
+++ LS.py
@@ -0,0 +1,28 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_borrowed = False
+
+    def borrow(self):
+        self.is_borrowed = True
+        print(self.title, "has been borrowed.")
+
+    def return_book(self):
+        self.is_borrowed = True
+        print(self.title, "has been borrowed.")
+
+
+# create 3 books
+book1 = Book("Harry Potter", "J.K. Rowling")
+book2 = Book("The Hobbit", "J.R.R. Tolkien")
+book3 = Book("The Gathering Blue", "Louis Lowery")
+
+book1.borrow()
+book2.borrow()
+
+book1.return_book()
+book2.return_book()
+
+book3.borrow()
+book3.return_book()
